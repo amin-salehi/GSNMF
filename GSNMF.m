7a4f126d5d0c8f4dea63a4a1048e8596cc8440b7
amin-salehi <asalehi1@asu.edu>
2018-05-03 16:05:44 -0700

    fourth commit

--- GSNMF.m
+++ GSNMF.m
@@ -35,12 +35,19 @@ minVal = 1e-1000;
 for it = 1:niter
     V = X * U * inv(U'*U);
     V(isnan(V))=0;
-    [XTV_pos, XTV_neg] = PosNegSeperation(X'*V);
-    [VTV_pos, VTV_neg] = PosNegSeperation(V'*V);
-    [lag_pos, lag_neg] = PosNegSeperation(U'*X'*V - V'*V  + lambda*U'*(Z)*U);
+    [XTV_pos, XTV_neg] = PosNegSeparation(X'*V);
+    [VTV_pos, VTV_neg] = PosNegSeparation(V'*V);
+    [lag_pos, lag_neg] = PosNegSeparation(U'*X'*V - V'*V  + lambda*U'*(Z)*U);
     U = U.*(((XTV_pos + U*VTV_neg + lambda*Z*U + U*lag_neg)./max((XTV_neg + U*VTV_pos + U*lag_pos),minVal)).^(0.5)); 
     U(isnan(U))=0;
 end
 loss = norm(X - V*U', 'fro')^2 + lambda*trace(U'*Z*U);
+
+function [ A_pos, A_neg ] = PosNegSeparation( A )
+    A_abs = abs(A);
+    A_pos = (A_abs + A) / 2;
+    A_neg = (A_abs - A) / 2;
+end
+
 end
 
